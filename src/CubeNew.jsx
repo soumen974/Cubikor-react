19d77a704aad6ad4d8ce87a3c286cf65cd4c6bfe
soumen bhunia <sob99338@gmail.com>
2023-05-30 10:47:41 +0530

    do a little

--- src/CubeNew.jsx
+++ src/CubeNew.jsx
@@ -13,16 +13,6 @@ import img11 from "./images-app/ad-cube-img-2.jpg";
 import img12 from "./images-app/ad-cube-img-3.jpg";
 
 
-// love svg
-
-{/* <svg
-                  viewBox="0 0 24 24"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-heart"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                </svg> */}
 
 
 export default function CubeNew() {
