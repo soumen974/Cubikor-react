d66abf3d298d92b13ae74d47910125148cc59d72
Soumen Bhunia <sob99338@gmail.com>
2023-12-10 01:24:32 +0530

    add a developer page

---
+++ src/Developer/DevLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DevLayout() {
+  return (
+    <div className='h-[100vh]  grid place-content-center bg-slate-800 text-white'>
+        this is developer mode 
+    </div>
+  )
+}
