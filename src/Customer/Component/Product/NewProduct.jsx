044d3c378b54dcc3aeaaebdb5c3a91a387691e97
Soumen Bhunia <sob99338@gmail.com>
2024-04-29 04:10:09 +0530

    sending product id to the overview page through uri  params

--- src/Customer/Component/Product/NewProduct.jsx
+++ src/Customer/Component/Product/NewProduct.jsx
@@ -17,31 +17,41 @@ export default function NewProduct(Props) {
   return (
     <div className=''>
     { !Props.page ? 
-    <div className=' px-[8rem] py-[5rem] flex'>
-        {product.breadcrumbs.map((breadcrumb) => (
-            <div key={breadcrumb.id}>
+    <div className=' pt-20 pb-7'>
+       <div className="  ">
+          <div className="  text-gray-900">
+          <nav aria-label="Breadcrumb">
+          <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            {product.breadcrumbs.map((breadcrumb) => (
+              <li key={breadcrumb.id}>
                 <div className="flex items-center">
-                <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                  <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
                     {breadcrumb.name}
-                </a>
-                <svg
+                  </a>
+                  <svg
                     width={16}
                     height={20}
                     viewBox="0 0 16 20"
                     fill="currentColor"
                     aria-hidden="true"
                     className="h-5 w-4 text-gray-300"
-                >
+                  >
                     <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-                </svg>
+                  </svg>
                 </div>
-            </div>
+              </li>
             ))}
-            <div className="text-sm">
-            <a href={product.href} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
+            <li className="text-sm">
+              <a href={product.href} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
                 {product.name}
-            </a>
-            </div>
+              </a>
+            </li>
+          </ol>
+        </nav>
+           
+          </div>
+        </div>
+        
 
             
     </div> : null}
