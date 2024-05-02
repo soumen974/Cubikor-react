8cb2c730e607a0ee778d094f0d3245c0edea3880
Soumen Bhunia <sob99338@gmail.com>
2024-05-03 01:28:39 +0530

    adding pagonation

--- src/Customer/Component/Product/CategoryProduct.jsx
+++ src/Customer/Component/Product/CategoryProduct.jsx
@@ -24,7 +24,7 @@ if (!selectedProduct ) {
 
   }
 
-  const pagenation = {
+  const product = {
     name: '3x3 cubes',
     breadcrumbs: [
       { id: 1, name: 'Home', href: '/' },
@@ -36,11 +36,42 @@ if (!selectedProduct ) {
       <QuickPreview CategoryId={PuzzleId} CubeId={CubeId} setOpen={setOpen} open={open} />
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
         
-        <div className="mt-20">
+        <nav aria-label="Breadcrumb" className="mt-10">
+          <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            {product.breadcrumbs.map((breadcrumb) => (
+              <li key={breadcrumb.id}>
+                <div className="flex items-center">
+                  <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                    {breadcrumb.name}
+                  </a>
+                  <svg
+                    width={16}
+                    height={20}
+                    viewBox="0 0 16 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    className="h-5 w-4 text-gray-300"
+                  >
+                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                  </svg>
+                </div>
+              </li>
+            ))}
+            <li className="text-sm">
+                  <a href="/new" aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
+                  New
+                  </a>
+                </li>
+            
+          </ol>
+        </nav>
+        
+
+        <div className="">
           {productData .filter(product => product.id === categoryId).map(product => (
             <div key={product.id} className="">
                 <h2 className="text-2xl font-bold tracking-tight text-gray-900 py-5">Category : {product.name}</h2>
-                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div className="mt-6 grid grid-cols-2 gap-x-3 gap-y-3 md:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     { product.data.map((item) => (
                         <div key={item.id} onClick={() => { setOpen(true); setPuzzleId(product.id);setCubeId(item.id); }} className="group cursor-pointer bg-gray-100 hover:bg-indigo-500 transform-bg duration-500 group px-2 py-2 rounded-lg relative">
                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
