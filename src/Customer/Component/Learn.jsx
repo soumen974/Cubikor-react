1f965815995d1869fee82592aecc870063d49d59
Soumen Bhunia <sob99338@gmail.com>
2024-05-16 13:21:05 +0530

    learn page comming

---
+++ src/Customer/Component/Learn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Learn() {
+  return (
+    <>
+    <h1 className='items-center flex justify-center p-20 h-screen'>Comming soon ..</h1>
+    </>
+  )
+}
