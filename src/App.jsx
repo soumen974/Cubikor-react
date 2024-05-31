9f653e8249f6319dea5faf7248190f495344a47a
Soumen Bhunia <sob99338@gmail.com>
2024-05-31 09:53:22 +0530

    seller and authenticated routes updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from "./Customer/Component/Layout";
 import Home from "./Customer/Pages/Home";
 import NotFound from './Customer/Component/NotFound';
 import NewProduct from './Customer/Component/Product/NewProduct';
-import SIgnIn from './Customer/Component/SignIn';
 import ProductOverview from './Customer/Component/Product/ProductOverviews';
 import CategoryProduct from './Customer/Component/Product/CategoryProduct';
 import Learn from './Customer/Component/Learn';
-import NowTry from './Customer/Component/Now Try';
+import Profile from "./Customer/Profile";
+import EditProfile from './Customer/Pages/EditProfile';
+import CreateCategory from './Seller/CreateCategory';
+import CreateProduct from "./Seller/CreateProduct";
+import Login from "./Seller/Login";
+import SignUp from "./Seller/SignUp";
+import LayoutSeller from './Seller/LayoutSeller';
+import Image from "./Seller/Image_uploder/Image";
+import ImageList from "./Seller/Image_uploder/ImageList";
+import ImageUploader from "./Seller/Image_uploder/ImageUploader";
+
 
 
 export default function App() {
 
+  const Authenticated =  localStorage.getItem('isUserAuthenticated');
+  const Seller =  localStorage.getItem('SellerToken');
+  
+
+  
  
+
+
   return (
     <>
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+
+          <Route path="/images" element={<Image/>} />
+          <Route path="/ImageList" element={<ImageList/>} />
+          <Route path="/ImageUploader" element={<ImageUploader/>} />
+          
           <Route path="/new" element={<NewProduct/>}/ >
           <Route path="/learn" element={<Learn/>}/ >
           <Route path="/productview/:CategoryId/:CubeId" element={<ProductOverview/>}/ >
           <Route path="/productview/" element={<ProductOverview/>}/ >
           <Route path="/products" element={<CategoryProduct/>}/ >
           <Route path="/products/:CategoryPageID" element={<CategoryProduct/>}/ >
+          {Authenticated ? (<Route path="/profile" element={<Profile/>} />): null}
+          {Authenticated ? (<Route path="/profileEdit" element={<EditProfile/>} />): null}
+          
+
           <Route path="*" element={<NotFound/>} />
         </Route>
+
+
+        {Seller ? (
+        <Route path="/seller" element={<LayoutSeller/>}>
+          <Route path="/seller/categoriesadd" element={<CreateCategory/>} />
+          <Route path="/seller/productadd" element={<CreateProduct/>} />
+
+        </Route>
+        ):
+        <Route path="/seller/Login" element={<Login/>} />
+        }
+        <Route path="/seller/Login" element={<Login/>} />
+        <Route path="/seller/SignUp" element={<SignUp/>} />
+
+        
+
+
         
 
       </Routes>
