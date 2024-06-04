0ec68932ef4206553637291c9fef59261da34e96
Soumen Bhunia <sob99338@gmail.com>
2024-06-04 22:27:29 +0530

    added cat and prod

--- src/Seller/DashBoard.jsx
+++ src/Seller/DashBoard.jsx
@@ -1,58 +1,52 @@
-import { Fragment } from 'react'
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
-} from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
 import Products from './Components/Products'
 import Category from './Components/Category'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function DashBoard() {
+  const product = {
+    name: '3x3 cubes',
+    breadcrumbs: [
+      { id: 1, name: 'Dashboard', href: '/seller' },
+    ]};
+
   return (
     <>
      
       <div className=" ">
-       
-
-        <header className="">
-          <div className="   ">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-          </div>
+        <header className="pb-6">
+          <ol role="list" className="mx-auto flex  items-center space-x-2  lg:px-5">
+          {product.breadcrumbs.map((breadcrumb) => (
+            <li key={breadcrumb.id}>
+              <div className="flex items-center">
+                <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                  {breadcrumb.name}
+                </a>
+                <svg
+                  width={16}
+                  height={20}
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="h-5 w-4 text-gray-300"
+                >
+                  <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                </svg>
+              </div>
+            </li>
+          ))}
+          </ol>
         </header>
-        <main className='grid overflow-hidden'>
+        <main className=' grid'>
           <div className="">
             <Category/>
           </div>
-          <div className="">
+          <div className=" overflow-x-auto">
           <Products/>
           </div>
         </main>
