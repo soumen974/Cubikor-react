fe038b944341e9b455cbec2426b1b37f5ef8adb3
Soumen Bhunia <sob99338@gmail.com>
2024-06-08 11:08:00 +0530

    redux slightly added for testing demo

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import systemDataReducer from './session';
 import userReducer from "./userSlice";
+import cartReducer from './cartSlice';
 
 const store = configureStore({
   reducer: {
     systemData: systemDataReducer,
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
