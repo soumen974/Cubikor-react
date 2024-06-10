d1e1fafcdf1f146cbba52f144aef820332b1d453
Soumen Bhunia <sob99338@gmail.com>
2024-06-11 00:15:14 +0530

    page updating

--- src/Customer/Component/Navigation/Navigation.jsx
+++ src/Customer/Component/Navigation/Navigation.jsx
@@ -426,13 +426,13 @@ useEffect(() => {
                   </div>
 
                   {/* Cart */}
-                  <div className="ml-4 flow-root lg:ml-6" onClick={isAuthenticated ? () => setshoppingCart(true) : () => setsignin(true)}  >
+                  <div className="relative ml-4 flow-root lg:ml-6" onClick={isAuthenticated ? () => setshoppingCart(true) : () => setsignin(true)}  >
                     <a href="#" className="group -m-2 flex items-center p-2">
                       <ShoppingBagIcon
                         className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                         aria-hidden="true"
                       />
-                      <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{isAuthenticated ? itemCount : null}</span>
+                      <span className="absolute left-1 bottom-2 ml-2 text-[0.8rem] font-medium text-gray-100 group-hover:text-gray-800 bg-indigo-500  px-[0.4rem] py-0 rounded-full">{isAuthenticated ? itemCount : null}</span>
                       <span className="sr-only">items in cart, view bag</span>
                     </a>
                   </div>
