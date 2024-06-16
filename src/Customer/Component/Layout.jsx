60fa6349fe146e3a5aa3a564cf501ae82da9a9dd
Soumen Bhunia <sob99338@gmail.com>
2024-06-16 22:48:17 +0530

    scroll to top button is working good

--- src/Customer/Component/Layout.jsx
+++ src/Customer/Component/Layout.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 import { Outlet } from 'react-router-dom'
 import Navigation from "./Navigation/Navigation";
 import Footer from './Footer';
-import PageLoder from '../../Loaders/PageLoder';
 
 
 export default function Layout() {
+
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+      const handleScroll = () => {
+          if (window.scrollY >= 500) { // Adjust the value as needed
+              setShowButton(true);
+          } else {
+              setShowButton(false);
+          }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      // Cleanup the event listener on component unmount
+      return () => {
+          window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
   return (
     <>
      <Navigation/>
-     <div className=" mx-auto max-w-screen py-20 px-2 md:px-[12vw]  ">
+     <div className="relative mx-auto max-w-screen py-20 px-2 md:px-[12vw]  ">
           <Outlet />
 
-          <button   onClick={()=>{window.scrollTo({top: 0, behavior: 'smooth'})}} className=" bg-gray-200 rounded-full  animate-bounce w-fit p-2">
-            <svg class="w-6 h-6 text-violet-500 rotate-180" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-              </svg>
-          </button>
-          <PageLoder/>
-      </div> 
+          
+      </div>
       
       <Footer/>
+      <abbr title='Scroll to Top' className={` fixed top-[47rem] ${showButton? "opacity-100 " :" opacity-0 invisible "} right-0  mx-auto max-w-screen py-20 px-2 md:px-[12vw]`}>
+        <button   onClick={()=>{window.scrollTo({top: 0, behavior: 'smooth'})}} className=" group shadow-md hover:shadow-indigo-300 bg-gray-200 rounded-full  animate-bounce w-fit p-2">
+                <svg class="w-6 h-6 text-violet-500  rotate-180" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                  </svg>
+          </button>
+      </abbr> 
     </>
   )
 }
