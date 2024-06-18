076bf6623a38997dd3787e3d28e41e24a08053dd
Soumen Bhunia <sob99338@gmail.com>
2024-06-19 01:09:35 +0530

    slider button added

--- src/Customer/Component/ProductCard.jsx
+++ src/Customer/Component/ProductCard.jsx
@@ -1,8 +1,9 @@
-import { useState,useEffect } from "react";
+import { useState,useEffect,useRef } from "react";
 import products from "./Data/products.json";
 import QuickPreview from "./Product/QuickPreview";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 
 
@@ -85,6 +86,23 @@ export default function ProductCard() {
         fetchProducts();
       }
     }, [shopId]);
+
+   
+    // slider
+    const sliderRef = useRef(null);
+
+    const handleNextClick = () => {
+      if (sliderRef.current) {
+        sliderRef.current.scrollBy({ left: 1090, behavior: 'smooth' }); // Adjust the scroll amount as needed
+      }
+      
+    };
+
+    const handlePrevClick = () => {
+      if (sliderRef.current) {
+        sliderRef.current.scrollBy({ left: -1090, behavior: 'smooth' }); // Adjust the scroll amount as needed
+      }
+    };
     
     
    
@@ -92,13 +110,37 @@ export default function ProductCard() {
   
   return (
     <>
+    <style jsx>{`
+      .scrollbar-hide::-webkit-scrollbar {
+        display: none; 
+      }
+      .scrollbar-hide {
+        -ms-overflow-style: none; 
+        scrollbar-width: none; 
+      } 
+    `}</style>
+       
+
       <QuickPreview ShopIDsend={ShopIDsend} CubeId={CubeId} catId={catId} setOpen={setOpen} open={open} />
-      <div className="isolate mx-auto max-w-2xl px-0 py- sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
+      
+      <div className="  relative  isolate mx-auto max-w-2xl px-0 py- sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">New Arrival</h2>
+        <abbr title="scroll right  " className="no-underline  hidden cursor-pointer absolute top-20 right-0 h-[2vw] w-6 hover:bg-indigo-500  bg-indigo-600 text-gray-100 px-4 py-5 justify-center lg:flex items-center " onClick={handleNextClick}>
+          <button>
+            <MdKeyboardArrowRight className="h-6 w-6"/>
+          </button> 
+        </abbr>
+
+        <abbr title="scroll right  " className="no-underline  hidden cursor-pointer absolute top-40 right-0 h-[2vw] w-6 hover:bg-indigo-500  bg-indigo-600 text-gray-100 px-4 py-5 justify-center lg:flex items-center " onClick={handlePrevClick}>
+          <button>
+            <MdKeyboardArrowRight className="h-6 w-6 rotate-180"/>
+          </button> 
+        </abbr>
+        {productdata ?
+        <div ref={sliderRef} className=" mt-6 grid grid-cols-2 gap-x-3 gap-y-3 md:grid-cols-2 lg:flex xl:overflow-x-auto scrollbar-hide   xl:gap-x-8">
         
-        <div className="mt-6 grid grid-cols-2 gap-x-3 gap-y-3 md:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {productdata.map((product) => (
-          <div  key={product.id} className="">
+          <div  key={product.id} className="slider-item">
             <div
               onClick={() => {
                 setOpen(true);
@@ -106,12 +148,11 @@ export default function ProductCard() {
                 setShopIDsend(product.shop_id);
                 setcatId(product.category_id);
                 
-                // setCubeId(product.data[product.data.length - 1].id);
               }}
-              className="group cursor-pointer bg-gray-100 hover:bg-indigo-500 transition duration-500 px-2 py-2 rounded-lg relative"
+              className="group cursor-pointer  bg-gray-100 hover:bg-indigo-500 transition duration-500 px-2 py-2  rounded-lg relative"
             >
-              {/* ${window.btoa(product.id)} */}
-              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+              
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:w-[14rem] lg:h-[17rem]">
                  <Link to={`/productview/${window.btoa(product.id*721426)}`}>
                 <img
                   src={product.imageSrc}
@@ -121,11 +162,12 @@ export default function ProductCard() {
                 </Link>
                 
               </div>
-              <div className="mt-4  justify-between">
-                  <div>
-                    <h3 className="text-sm group-hover:text-white text-gray-700">
-                      {product.name}
-                    </h3>
+              <div className="mt-4   justify-between">
+                  <div className="  " >
+                    <p className="text-sm md:w-[14rem] truncate overflow-hidden group-hover:text-white text-gray-700 w-full ">
+                    {product.name}
+                    </p>
+                
                     <p className="mt-1 py-1 text-sm group-hover:text-yellow-300 text-gray-500">
                       {product.color}
                     </p>
@@ -139,8 +181,129 @@ export default function ProductCard() {
         ))}
 
 
+        </div>:
+        <>
+        <div className="mt-6 animate-pulse grid grid-cols-2 gap-x-3 gap-y-3 md:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <div  className="">
+            <div
+              
+              className="group cursor-pointer bg-gray-100 hover:bg-indigo-500 transition duration-500 px-2 py-2 rounded-lg relative"
+            >
+              
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                
+                <div
+                  className="w-full h-full object-cover object-center lg:w-full lg:h-full transition-transform duration-500 group-hover:scale-105 rounded-sm"
+                />
+              
+                
+              </div>
+              <div className="mt-4  justify-between">
+                  <div>
+                    <h3 className="text-sm group-hover:text-white w-fit px-[6vw] py-[0.3rem] rounded-full bg-gray-300">
+                      
+                    </h3>
+                    <p className="mt-1  text-sm group-hover:text-yellow-300 px-9 py-[0.3rem] w-fit rounded-full bg-gray-300">
+                    </p>
+                  </div>
+                <p className="mt-2 text-sm w-fit px-8 py-4 font-medium bg-yellow-400 rounded-md text-gray-900">
+                
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div  className="">
+            <div
+              
+              className="group cursor-pointer bg-gray-100 hover:bg-indigo-500 transition duration-500 px-2 py-2 rounded-lg relative"
+            >
+              
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                
+                <div
+                  className="w-full h-full object-cover object-center lg:w-full lg:h-full transition-transform duration-500 group-hover:scale-105 rounded-sm"
+                />
+              
+                
+              </div>
+              <div className="mt-4  justify-between">
+                  <div>
+                    <h3 className="text-sm group-hover:text-white w-fit px-[6vw] py-[0.3rem] rounded-full bg-gray-300">
+                      
+                    </h3>
+                    <p className="mt-1  text-sm group-hover:text-yellow-300 px-9 py-[0.3rem] w-fit rounded-full bg-gray-300">
+                    </p>
+                  </div>
+                <p className="mt-2 text-sm w-fit px-8 py-4 font-medium bg-yellow-400 rounded-md text-gray-900">
+                
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div  className="">
+            <div
+              
+              className="group cursor-pointer bg-gray-100 hover:bg-indigo-500 transition duration-500 px-2 py-2 rounded-lg relative"
+            >
+              
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                
+                <div
+                  className="w-full h-full object-cover object-center lg:w-full lg:h-full transition-transform duration-500 group-hover:scale-105 rounded-sm"
+                />
+              
+                
+              </div>
+              <div className="mt-4  justify-between">
+                  <div>
+                    <h3 className="text-sm group-hover:text-white w-fit px-[6vw] py-[0.3rem] rounded-full bg-gray-300">
+                      
+                    </h3>
+                    <p className="mt-1  text-sm group-hover:text-yellow-300 px-9 py-[0.3rem] w-fit rounded-full bg-gray-300">
+                    </p>
+                  </div>
+                <p className="mt-2 text-sm w-fit px-8 py-4 font-medium bg-yellow-400 rounded-md text-gray-900">
+                
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div  className="">
+            <div
+              
+              className="group cursor-pointer bg-gray-100 hover:bg-indigo-500 transition duration-500 px-2 py-2 rounded-lg relative"
+            >
+              
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                
+                <div
+                  className="w-full h-full object-cover object-center lg:w-full lg:h-full transition-transform duration-500 group-hover:scale-105 rounded-sm"
+                />
+              
+                
+              </div>
+              <div className="mt-4  justify-between">
+                  <div>
+                    <h3 className="text-sm group-hover:text-white w-fit px-[6vw] py-[0.3rem] rounded-full bg-gray-300">
+                      
+                    </h3>
+                    <p className="mt-1  text-sm group-hover:text-yellow-300 px-9 py-[0.3rem] w-fit rounded-full bg-gray-300">
+                    </p>
+                  </div>
+                <p className="mt-2 text-sm w-fit px-8 py-4 font-medium bg-yellow-400 rounded-md text-gray-900">
+                
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        </>}
+
       </div>
+      
     </>
   );
 }
