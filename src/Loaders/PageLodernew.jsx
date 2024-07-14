33ece7c69cfe65609ce1e8c41d0b4ec10b001226
Soumen Bhunia <sob99338@gmail.com>
2024-07-14 16:24:18 +0530

    updated

---
+++ src/Loaders/PageLodernew.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PageLoder from "./PageLoder";
+export default function PageLodernew() {
+  return (
+    <div className='bg-gray-00 z-[999] absolute top-1/2 left-1/2 flex justify-center items-center'>
+        <PageLoder />
+    </div>
+  )
+}
