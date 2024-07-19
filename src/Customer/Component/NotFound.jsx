2ec01c018b88cdb011ca5c8e98b28400ce8101f5
Soumen Bhunia <sob99338@gmail.com>
2024-07-19 20:27:33 +0530

    svg updated

--- src/Customer/Component/NotFound.jsx
+++ src/Customer/Component/NotFound.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
-
+import notFoundimg from "./Data/images-app/undraw_page_not_found_re_e9o6.svg";
 export default function NotFound() {
   document.title = "404 | Page not found"
     return (
       <>
-        <main className="grid h-[100vh] place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <main className="grid h-[70vh] place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
           <div className="text-center">
-            <p className="text-2xl font-semibold text-indigo-600">404</p>
+            <div className="text-2xl flex justify-center items-center font-semibold text-indigo-600">
+              <img src={notFoundimg} className="h-40 sm:h-60  w-40 sm:w-60" alt="" />
+            </div>
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
             <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
